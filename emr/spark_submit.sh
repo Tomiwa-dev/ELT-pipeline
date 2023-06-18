d6e922d12a366dc46f221ddfa9490f2c370b86c8
Tomiwa <adewoletomiwa@gmail.com>
2023-06-18 14:17:58 +0100

    Update spark_submit.sh

--- emr/spark_submit.sh
+++ emr/spark_submit.sh
@@ -2,4 +2,3 @@ spark-submit --master 'local[*]' --jars '/Users/emmanuel/Documents/Alerzo/dbtPre
 
 
 
-#spark-submit --master 'local[*]' --jars /Users/emmanuel/jarfiles/mongo-spark-connector-10.0.1.jar --packages org.mongodb.spark:mongo-spark-connector:10.0.1 '/Users/emmanuel/Documents/personal_projects/dbtProject/emr/mongoToS3.py' --mongouri mongodb+srv://tomiwa-dev:Z2zcvaYMLhLbkxwj@cluster0.t5mqmz2.mongodb.net/ --database sample_airbnb --collection listingsAndReviews
